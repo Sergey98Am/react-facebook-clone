5e6904a043c0ea6339c60fbaf849cc02626a1b98
Serg <sergey.gabrielyan1998@gmail.com>
2023-05-13 15:53:15 +0400

    Dark mode fixes

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -4,7 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 
 export const Feed = () => {
   return (
-    <Box component="main" p={{xs: 2, xl: 8}} sx={{ flexGrow: 1 }}>
+    <Box component="main" py={4} px={{xs: 2, xl: 16}} sx={{ flexGrow: 1 }}>
       <Toolbar />
       <Post />
       <Post />
