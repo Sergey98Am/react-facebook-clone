d5f552508087480f40eb3cf5d71d285fc668bcc9
Serg <sergey.gabrielyan1998@gmail.com>
2023-05-13 15:27:30 +0400

    Add dark mode

--- src/App.js
+++ src/App.js
@@ -3,27 +3,34 @@ import { Feed } from "./components/Feed/Feed";
 import { Rightbar } from "./components/Rightbar/Rightbar";
 import { Sidebar } from "./components/Sidebar";
 import { Navbar } from "./components/Navbar/Navbar";
-import { useTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme, useTheme } from "@mui/material/styles";
 import { Add } from "./components/CreatePost/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
 
   const theme = useTheme();
-  const biggerThanSm = useMediaQuery(theme.breakpoints.up('sm'));
-  const biggerThanLg = useMediaQuery(theme.breakpoints.up('lg'));
+  const biggerThanSm = useMediaQuery(theme.breakpoints.up("sm"));
+  const biggerThanLg = useMediaQuery(theme.breakpoints.up("lg"));
 
   return (
-    <div>
-      <Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
         <Navbar biggerThanLg={biggerThanLg} />
-        <Box sx={{ display: 'flex' }}>
-          {biggerThanSm && <Sidebar />}
+        <Box sx={{ display: "flex" }}>
+          {biggerThanSm && <Sidebar mode={mode} setMode={setMode} />}
           <Feed />
           {biggerThanLg && <Rightbar />}
         </Box>
         <Add />
       </Box>
-    </div>
+    </ThemeProvider>
   );
 }
 
